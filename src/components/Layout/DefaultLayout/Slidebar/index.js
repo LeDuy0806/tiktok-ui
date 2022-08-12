35ec25e7a9a2a786bcf2b2910884f8c7a0d719de
Van Duy <duylangtu931@gmail.com>
2022-08-12 23:53:40 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/DefaultLayout/Slidebar/index.js
+++ src/components/Layout/DefaultLayout/Slidebar/index.js
@@ -1,5 +1,12 @@
+import classNames from 'classnames/bind'
+import styles from './Slidebar.module.scss'
+
+const cx = classNames.bind(styles)
+
 function Slidebar() {
-  return <h2>SlideBar</h2>;
+  return <aside className={cx('wrapper')}>
+    <h2>Slidebar</h2>
+  </aside>
 }
 
 export default Slidebar;
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
+import classNames from 'classnames/bind'
 import Header from '~/components/Layout/components/Header';
 import Slidebar from './Slidebar';
+import styles from './DefaultLayout.module.scss'
+
+const cx = classNames.bind(styles)
 
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div className="container">
+      <div className={cx('container')}>
         <Slidebar />
-        <div className="container">{children}</div>
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
